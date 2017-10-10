d662ff08e819e1ffcfd7994b6ba05f9eb84f58fd
lenovo <583889796@qq.com>
2017-10-10 20:50:33 +0800

    frist commit

---
+++ src/com/trs/ts1/TestMethod.java
@@ -0,0 +1,21 @@
+package com.trs.ts1;
+
+import java.util.logging.Logger;
+
+public class TestMethod {
+    private static Logger log = Logger.getLogger(String.valueOf(TestMethod2.class));
+    public static void main(String args[]){
+        String str = "12tg37t67edg3uy";
+        StringBuffer sb = new StringBuffer();
+        for (int i=0;i<str.length()-1;i++) {
+            int c = str.charAt(i);
+            if (c>48 && c<57) {
+                sb.append((char)c);
+        }
+        }
+        System.out.print(sb);
+        int c = 10/3;
+        System.out.print(c);
+        log.info("调用时间:");
+    }
+}
